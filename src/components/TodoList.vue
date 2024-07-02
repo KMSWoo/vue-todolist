5d58a5df66ca753736e23a70d868e50318571c5e
Beaman <wayneincode@gmail.com>
2024-07-02 18:42:22 +0800

    first commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <el-button type="primary" round>主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  props: {
+    msg: String
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/element.js'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// import { Button, Select } from 'element-ui'
+// Vue.use(Button)
+// Vue.use(Select)
 
 Vue.config.productionTip = false
 
