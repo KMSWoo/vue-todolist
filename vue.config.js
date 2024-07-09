db6c7642089b322c2a873e2963d39a16234cefb6
--global <wayneincode@gmail.com>
2024-07-09 19:36:59 +0800

    ...

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,4 @@
 const { defineConfig } = require('@vue/cli-service');
-const BundleAnalyzerPlugin =
-  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
 module.exports = defineConfig({
   transpileDependencies: true,
-  configureWebpack: {
-    plugins: [new BundleAnalyzerPlugin()],
-  },
 });
